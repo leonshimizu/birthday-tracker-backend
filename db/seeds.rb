22bd43ddc94aced598a56126df5e4189723d6236
“leonshimizu” <lmshimizu@gmail.com>
2021-11-11 14:22:41 -0800

    added to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 Birthday.create!([
   {name: "Stassie Shimizu", month: "2", day: "19", year: "2019"},
-  {name: "Clai Crisostomo", month: "3", day: "5", year: "1998"},
   {name: "Leon Shimizu", month: "7", day: "20", year: "1999"},
-  {name: "Leon", month: "07", day: "20", year: "1999"},
-  {name: "John", month: "03", day: "24", year: "1968"},
-  {name: "Dafne Shimizu", month: "9", day: "26", year: "1973"}
+  {name: "Dafne Shimizu", month: "9", day: "26", year: "1973"},
+  {name: "Leon Shimizu", month: "07", day: "20", year: "1999"},
+  {name: "John Shimizu", month: "03", day: "24", year: "1968"},
+  {name: "Clai Crisostomo (Shimizu)", month: "3", day: "5", year: "1998"}
 ])
